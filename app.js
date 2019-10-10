d1a1342e5198335d2cb3f4040f23e06673d4336f
mostafamohamedmostafa <dev.mostafa.playstore@gmail.com>
2019-10-10 11:00:31 +0200

    building the main js project body by creating 3 modules

---
+++ app.js
@@ -0,0 +1,26 @@
+// Start With Data - Budget Module
+
+var budgetController = (function()
+{
+// Some COde
+}
+)();
+
+
+
+
+// Ui Controller - Show the Data TO INterface Module
+
+var UIController = (function(){
+
+    // Some Code
+})();
+
+
+// APP Controller 
+
+var Controller = (function(budgetCtrl,UICtrl){
+
+    // Some Code
+    // Connection betwwen 2 other modules
+})(budgetController,UIController);
